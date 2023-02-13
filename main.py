4eb2a498e92527e7fb152ddca730f676f334b482
Akim <akimkom@protonmail.com>
2023-02-13 21:35:21 +0300

    Done one-way inversion

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def main():
     for i in range (5):
         t.append(turtle.Turtle())
         t[i].pensize(3)
-        t[i].speed(0)
+        t[i].speed(10)
 
     c1 = Circle(0, 0, 100)
     c2 = Circle(200, 200, 50)
-    c3 = Circle(400, -100, 75)
+    c3 = Circle(425, -100, 75)
     circles = [c1, c2, c3]
 
     draw_circles(circles, t)
@@ -22,39 +22,91 @@ def main():
     d_r_13 = find_d_r(c1, c3)
     d_r_23 = find_d_r(c2, c3)
     d_r = min(d_r_12, d_r_13, d_r_23)
+
+    # infl_c1 and infl_c2 are the ones that are expanded until tangency.
     infl_c1 = c1
     infl_c2 = c2
+    infl_c3 = c3
     if d_r == d_r_13:
         infl_c2 = c3
+        infl_c3 = c2
     elif d_r == d_r_23:
         infl_c1 = c3
+        infl_c3 = c1
 
     # Inflate all three circles by delta r
     for i, circle in enumerate(circles):
         circle.r += d_r
         t[i].clear()
     draw_circles(circles, t)
-    D(f"\nd_r = {d_r}\nr1 = {infl_c1.r}, (0, 0)\nr2 = {infl_c2.r}, (200, 200)")
+    D(f"\nd_r = {d_r}\nr1 = {infl_c1.r}, ({infl_c1.x}, {infl_c1.y})\nr2 = {infl_c2.r}, ({infl_c2.x}, {infl_c2.y})")
 
-    angle_infl_c1_c2 = math.atan((infl_c1.y - infl_c2.y)/(infl_c1.x - infl_c2.x))
-    dist_x_inv = (infl_c1.r + d_r)*math.cos(angle_infl_c1_c2)
-    dist_y_inv = (infl_c1.r + d_r)*math.sin(angle_infl_c1_c2)
+    # Find the centre of the circle of inversion
+    k = infl_c1.r/(infl_c1.r + infl_c2.r)
+    x_inv = infl_c1.x + k*(infl_c2.x - infl_c1.x)
+    y_inv = infl_c1.y + k*(infl_c2.y - infl_c1.y)
 
-    if infl_c1.x < infl_c2.x:
-        x_inv = infl_c1.x + dist_x_inv
-    else:
-        x_inv = infl_c1.x - dist_x_inv
+    D(f"\nx_i = {x_inv}\ny_i = {y_inv}\nr = 300")
 
-    if infl_c1.y < infl_c2.y:
-        y_inv = infl_c1.y + dist_y_inv
-    else:
-        y_inv = infl_c1.y - dist_y_inv
-
-    D(f"\nx_i = {x_inv}\ny_i = {y_inv}")
-    c_inv = Circle(x_inv, y_inv, 800)
+    c_inv = Circle(x_inv, y_inv, 300)
+    t[3].pencolor("red")
     c_inv.draw(t[3])
 
+    infl_c1_inv_x, infl_c1_inv_y = find_inverse_point_x_y_of_tangent_circles(infl_c1, c_inv)
+    print("First line")
+    print(infl_c1_inv_x, infl_c1_inv_y)
+    infl_c2_inv_x, infl_c2_inv_y = find_inverse_point_x_y_of_tangent_circles(infl_c2, c_inv)
+
+    setpos_turtle_wo_drawing(t[0], x_inv, y_inv)
+    t[0].clear()
+    draw_inverted_circle_line(infl_c1_inv_x, infl_c1_inv_y, c_inv.r*2, t[0])
+
+    setpos_turtle_wo_drawing(t[1], x_inv, y_inv)
+    t[1].clear()
+    draw_inverted_circle_line(infl_c2_inv_x, infl_c2_inv_y, c_inv.r*2, t[1])
+
+    infl_c3_inv_x, infl_c3_inv_y = find_inverse_point_x_y(infl_c3, c_inv)
+    infl_c3_inv_r = find_radius_of_inverted_circle(infl_c3, c_inv)
+    c3_inv = Circle(infl_c3_inv_x, infl_c3_inv_y, infl_c3_inv_r)
+    t[2].clear()
+    c3_inv.draw(t[2])
+
     turtle.done()
+
+def find_radius_of_inverted_circle(c1, c_inv):
+    k = abs((c_inv.r**2) / ((c1.x - c_inv.x)**2 + (c1.y - c_inv.y)**2 - c1.r**2))
+    inv_r = k * c1.r 
+    return inv_r
+
+def setpos_turtle_wo_drawing(t, x, y):
+    t.penup()
+    t.setpos(x, y)
+    t.pendown()
+
+def draw_inverted_circle_line(x, y, length, turtle):
+    t = turtle
+    t.setheading(t.towards(x, y))
+    setpos_turtle_wo_drawing(t, x, y)
+    t.left(90)
+    t.forward(length)
+    t.setpos(x, y)
+    t.back(length)
+
+# Same as `find_inverse_point_x_y()` but w/o `- c1.r**2` because then
+# it goes off to infinity for reasons I haven't figured out yet.
+def find_inverse_point_x_y_of_tangent_circles(c1, c_inv):
+    k = abs((c_inv.r**2) / ((c1.x - c_inv.x)**2 + (c1.y - c_inv.y)**2))
+    c1_inv_x = c_inv.x + k*(c1.x - c_inv.x)
+    c1_inv_y = c_inv.y + k*(c1.y - c_inv.y)
+
+    return c1_inv_x, c1_inv_y
+
+def find_inverse_point_x_y(c1, c_inv):
+    k = abs((c_inv.r**2) / ((c1.x - c_inv.x)**2 + (c1.y - c_inv.y)**2 - c1.r**2))
+    c1_inv_x = c_inv.x + k*(c1.x - c_inv.x)
+    c1_inv_y = c_inv.y + k*(c1.y - c_inv.y)
+
+    return c1_inv_x, c1_inv_y
 
 def draw_circles(circles, turtles):
     for i, circle in enumerate(circles): 
@@ -84,6 +136,7 @@ class Circle():
         t.circle(self.r)
         t.penup()
         t.setpos(self.x, self.y)
+        t.pendown()
 
 if __name__ == '__main__':
     pretty_errors.configure(
